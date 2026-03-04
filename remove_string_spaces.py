9051dececc42dda1a16c0c3b2726c64fc35d35f6
Syntaxamurai <justinabwunza@gmail.com>
2026-03-04 15:35:00 +0300

    remove string spaces

---
+++ remove_string_spaces.py
@@ -0,0 +1,6 @@
+def no_space(x):
+    return x.replace(" ", "")
+
+print(no_space('w8 ozc2  s4dy5rcse1  zrd bftta01zf'))
+print(no_space('shy63o 6c kqgl ukzvx 55  zjjsq3n g'))
+print(no_space('g9oe7970fc tw7doxkn8qf393  49qziwf'))
