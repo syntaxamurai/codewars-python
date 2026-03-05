dfe0283076718f7f7853afbb1c5c1a1d67e81b5b
Syntaxamurai <justinabwunza@gmail.com>
2026-03-05 22:57:23 +0300

    how I solved the remove string spaces challenge

--- remove_string_spaces.py
+++ remove_string_spaces.py
@@ -1,6 +1,10 @@
 def no_space(x):
     return x.replace(" ", "")
 
+'''
+In this challenge, I use the replace function to replace any space with an empty string
+'''
+
 print(no_space('w8 ozc2  s4dy5rcse1  zrd bftta01zf'))
 print(no_space('shy63o 6c kqgl ukzvx 55  zjjsq3n g'))
 print(no_space('g9oe7970fc tw7doxkn8qf393  49qziwf'))
