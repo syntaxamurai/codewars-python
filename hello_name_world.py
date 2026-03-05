cdd9512c03c4a887707fe5983334981b3377f83b
Syntaxamurai <justinabwunza@gmail.com>
2026-03-05 22:55:35 +0300

    how I solved the hello name/world challenge

--- hello_name_world.py
+++ hello_name_world.py
@@ -5,6 +5,12 @@ def hello(name=""):
     else:
         return f"Hello, {name.capitalize()}!"
 
+'''
+This code uses the if...else conditional
+It first checks if the name is empty, then returns Hello, World!
+If the name is not empty, it returns an f-string
+It also capitalizes the first letter and the other letters are lowercased
+'''
 
 print(hello("John"))
 print(hello("aLIce"))
