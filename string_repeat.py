ce561f9a7eb991b03bdc96365059abaf607d5e12
Syntaxamurai <justinabwunza@gmail.com>
2026-03-05 23:33:40 +0300

    string repeat

---
+++ string_repeat.py
@@ -0,0 +1,13 @@
+def repeat_str(repeat, string):
+    return repeat * string
+
+# This solution multiplies the string by the number of times indicated to repeat the string
+
+print(repeat_str(4, 'a'))
+print(repeat_str(3, 'hello '))
+print(repeat_str(2, 'abc'))
+print(repeat_str(0, ''))
+print(repeat_str(0, 'I'))
+print(repeat_str(5, ''))
+print(repeat_str(6, 'I'))
+print(repeat_str(5, 'Hello'))
