4ea2871572c8a10eb2d5b75c8864213ef5a55cb6
Syntaxamurai <justinabwunza@gmail.com>
2026-03-05 23:40:06 +0300

    Opposite number

---
+++ opposite_number.py
@@ -0,0 +1,13 @@
+# Very simple, given a number, find its opposite (additive inverse).
+
+def opposite(number):
+  return number * -1
+
+#I solved this by multiplying the number by negative 1, which is it's opposite
+
+print(opposite(1))
+print(opposite(25.6))
+print(opposite(0))
+print(opposite(1425.2222))
+print(opposite(-3.1458))
+print(opposite(-95858588225))
